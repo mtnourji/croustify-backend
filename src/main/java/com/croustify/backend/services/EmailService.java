da58ed2a286d801928d3a378e4ab0b39e1032038
fofo-byte <alfonso.casucci@gmail.com>
2025-01-15 17:11:33 +0100

    Ticket CRS-30 Email notification for contact form

--- src/main/java/com/croustify/backend/services/EmailService.java
+++ src/main/java/com/croustify/backend/services/EmailService.java
@@ -1,14 +1,7 @@
 package com.croustify.backend.services;
 
 import com.croustify.backend.dto.ContactDTO;
-import com.croustify.backend.models.FoodTruckOwner;
-import com.croustify.backend.models.UserCredential;
 
 public interface EmailService {
-
-
-
-    void sendEmailConfirmation(FoodTruckOwner foodTruckOwnerDTO, UserCredential userCredential);
-
     void sendEmailNotificationContact(ContactDTO contactDTO);
 }
